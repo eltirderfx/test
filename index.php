6e6b3320e37cd44c7bbd2f9782f32974fd6bb517
elwandytirtana <eltirderfx@gmail.com>
2020-04-18 17:59:44 +0700

    menambah deskripsi lorem

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello, world !</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex itaque ea unde, voluptatibus rerum fugit sequi rem, ab esse quibusdam iure tempore! Exercitationem, at? Mollitia at debitis enim molestias nisi?</p>
 </body>
 </html>
